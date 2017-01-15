761140058b4779a0bf099e2def74ffc92e5babb1
John Crossley <jonnysnip3r@gmail.com>
2017-01-15 01:10:17 +0000

    Basic routing

--- banshee.gemspec
+++ banshee.gemspec
@@ -34,5 +34,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "minitest", "~> 5.0"
 
-  spec.add_runtime_dependency "rack"
+  spec.add_runtime_dependency 'rack', '~> 2.0', '>= 2.0.1'
 end
--- lib/banshee.rb
+++ lib/banshee.rb
@@ -3,7 +3,25 @@ require "banshee/version"
 module Banshee
   class Application
     def call(env)
-      [200, {"Content-type" => "text/html"}, ["Hello, world!"]]
+      if env["PATH_INFO"] == "/"
+        return [ 302, { "Location" => "/hello/world" }, [] ]
+      end
+
+      return [ 500, {}, [] ] if env["PATH_INFO"] == "/favicon.ico"
+
+      # env["PATH_INFO"] = "/hello/world" => HelloController.send(:world)
+      controller, action = get_controller_and_action(env)
+      response = controller.new.send(action)
+      [ 200, {"Content-type" => "text/html"}, [ response ] ]
     end
+  
+    def get_controller_and_action(env)
+      p env
+      _, controller, action = env["PATH_INFO"].split("/")
+
+      controller = "#{controller.capitalize}Controller"
+      [ Object.const_get(controller), action ]
+    end
+
   end
 end
